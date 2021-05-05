dc03b0521d99a98cd0bea0a07801c5b5229594ff
rauzola <raul_sigoli@hotmail.com>
2021-05-05 19:38:39 -0300

    dashgo

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import { FormControl,
+    FormLabel,
+    Input as ChakaraInput,
+    InputProps as ChakaraInputProps
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakaraInputProps {
+    name: string;
+    label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+    return (
+        <FormControl>
+        { !!label && <FormLabel htmlFor={name} >{label}</FormLabel> }
+
+        <ChakaraInput
+          name={name}
+          id={name}
+          type="email"
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          size="lg"
+          {...rest}
+        />
+      </FormControl>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,64 @@
+import {
+  Flex,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail"/>
+          <Input name="password" type="password" label="Senha"/>
+
+         
+
+          {/* <FormControl>
+            <FormLabel htmlFor="password" >Senha</FormLabel>
+
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl> */}
+          </Stack>
+
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+          >
+            Entrar
+        </Button>
+
+       
+
+
+      </Flex>
+    </Flex>
+  )
+}
