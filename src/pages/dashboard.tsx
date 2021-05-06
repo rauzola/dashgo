d629cdc5316aa4ad0f0f0c513fc7d86200310101
rauzola <raul_sigoli@hotmail.com>
2021-05-05 21:29:54 -0300

    test1

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -26,10 +26,10 @@ const options = {
     enabled: false,
   },
   tooltip: {
-    enabled: true,
+    enabled: false,
   },
   xaxis: {
-    type: "datetime",
+    type: 'datetime',
     axisBorder: {
       color: theme.colors.gray[600],
     },
@@ -37,28 +37,28 @@ const options = {
       color: theme.colors.gray[600],
     },
     categories: [
-      "2021-02-18T00:00:00.000Z",
-      "2021-02-19T00:00:00.000Z",
-      "2021-02-20T00:00:00.000Z",
-      "2021-02-21T00:00:00.000Z",
-      "2021-02-22T00:00:00.000Z",
-      "2021-02-23T00:00:00.000Z",
-      "2021-02-24T00:00:00.000Z",
+      '2021,03-18T00:00:00.000Z',
+      '2021,03-19T00:00:00.000Z',
+      '2021,03-20T00:00:00.000Z',
+      '2021,03-21T00:00:00.000Z',
+      '2021,03-22T00:00:00.000Z',
+      '2021,03-23T00:00:00.000Z',
+      '2021,03-24T00:00:00.000Z',
     ],
   },
   fill: {
     opacity: 0.3,
-    type: "gradient",
+    type: 'gradient',
     gradient: {
-      shade: "dark",
+      shade: 'dark',
       opacityFrom: 0.7,
       opacityTo: 0.3,
     },
   },
 };
 
-const series = [{ name: "series", data: [31, 120, 10, 28, 51, 18, 109] }];
-const series1 = [{ name: "series1", data: [311, 120, 10, 28, 51, 18, 109] }];
+const series = [{ name: 'series1', data: [31, 120, 10, 28, 61, 18, 109] }];
+const series1 = [{ name: 'series1', data: [312, 120, 10, 28, 61, 18, 109] }];
 
 export default function Dashboard() {
   return (
@@ -75,25 +75,17 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box
-            p="8"
-            bg="gray.800"
-            borderRadius={8}
-            pb="4"
-          >
-            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
-            <Chart options={options} series={series} type="numeric" height={160} />
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
-
-          <Box
-            p="8"
-            bg="gray.800"
-            borderRadius={8}
-            pb="4"
-          >
-            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-            <Chart options={options} series={series1} type="numeric" height={160} />
-
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
       </Flex>
