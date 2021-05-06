b63a2f809644e548c8b31c00261373fb3e5de02e
rauzola <raul_sigoli@hotmail.com>
2021-05-05 21:24:47 -0300

    numeric

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
             pb="4"
           >
             <Text fontSize="lg" mb="4">Inscritos da semana</Text>
-            <Chart options={options} series={series} type="area" height={160} />
+            <Chart options={options} series={series} type="numeric" height={160} />
           </Box>
 
           <Box
@@ -92,7 +92,7 @@ export default function Dashboard() {
             pb="4"
           >
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-            <Chart options={options} series={series1} type="area" height={160} />
+            <Chart options={options} series={series1} type="numeric" height={160} />
 
           </Box>
         </SimpleGrid>
