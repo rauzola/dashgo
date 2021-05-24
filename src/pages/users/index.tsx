1e13ed4863ee86cfdc9719234ce9a1f775ac50ca
rauzola <raul_sigoli@hotmail.com>
2021-05-23 22:42:22 -0300

    Listagem responsiva

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,159 +1,80 @@
-import { 
-    Box, 
-    Flex, 
-    Heading, 
-    Table, 
-    Icon, 
-    Button, 
-    Thead, 
-    Tr, 
-    Checkbox, 
-    Th, 
-    Tbody, 
-    Td, 
-    Text
-} from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
+import {Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue} from '@chakra-ui/react'
+import {Header} from '../../components/Header/index';
+import {RiAddLine, RiPencilLine} from 'react-icons/ri';
+import React from 'react';
+import { Pagination } from '../../components/Pagination';
+import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Box>
             <Header />
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+            <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
                 <Sidebar />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
-
-                       <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="pink"
-                          leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                <Box flex='1' borderRadius={8} bg='gray.800' p='8' >
+                    <Flex mb='8' justify='space-between' align='center'>
+                        <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+                        
+                        <Button 
+                            as='a'
+                            size='sm'
+                            fontSize='sm'
+                            colorScheme='pink'
+                            leftIcon={<Icon as={RiAddLine} fontSize='20' />}
                         >
                             Criar novo
                         </Button>
+
                     </Flex>
 
-                    <Table colorScheme="witeAlpha">
+                    <Table colorScheme='whiteAlpha'>
                         <Thead>
-                            <Tr px="6" color="gray.300" width="8">
-                                <Th px="6" color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
+                            <Tr>
+                                <Th px={['4','4','6']} color='gray.300' width='8'>
+                                    <Checkbox colorScheme='pink'/>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de Cadastro</Th>
-                                <Th width="8"></Th>
+                                    { isWideVersion && <Th>Data de cadastro</Th>}
+                                <Th width='8'></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
-
-
-                        <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
+                            <Tr>
+                                <Td px={['4','4', '6']}>
+                                    <Checkbox colorScheme='pink' />
                                 </Td>
                                 <Td>
                                     <Box>
-                                        <Text fontWeight="bold">Raul Sigoli</Text>
-                                        <Text fontSize="sm" color="gray.300">raul_sigoli@hotmail.com</Text>
+                                        <Text fontWeight='bold'>Raul Sigoli</Text>
+                                        <Text fontSize='sm' color='gray.300'>raul_sigoli@hotmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de Abril, 2021</Td>
-                                <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button>
-                                </Td>
-                            </Tr><Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Raul Sigoli</Text>
-                                        <Text fontSize="sm" color="gray.300">raul_sigoli@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>04 de Abril, 2021</Td>
-                                <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button>
-                                </Td>
-                            </Tr><Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Raul Sigoli</Text>
-                                        <Text fontSize="sm" color="gray.300">raul_sigoli@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>04 de Abril, 2021</Td>
-                                <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button>
-                                </Td>
-                            </Tr><Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Raul Sigoli</Text>
-                                        <Text fontSize="sm" color="gray.300">raul_sigoli@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>04 de Abril, 2021</Td>
-                                <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button>
-                                </Td>
+                                    { isWideVersion && <Td>27 de abril de 2021</Td>}
                             </Tr>
-
-
+                            <Tr>
+                                <Td px={['4','4', '6']}>
+                                    <Checkbox colorScheme='pink' />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'>Raul Sigoli</Text>
+                                        <Text fontSize='sm' color='gray.300'>raul_sigoli@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                    { isWideVersion && <Td>27 de abril de 2021</Td>}
+                            </Tr>
                         </Tbody>
                     </Table>
-
-
                     <Pagination />
                 </Box>
-
             </Flex>
         </Box>
-    );
+    )
 }
