66961c97c9dfdf24fe4a1d75993159f5ea812977
rauzola <raul_sigoli@hotmail.com>
2021-05-23 22:18:54 -0300

    Dashboard responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -74,7 +74,11 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius={8}
+          >
             <Text fontSize="large" mb="4">
               Inscritos da Semana
             </Text>
