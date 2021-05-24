2d4446166c6db8bcc8d1694aa9584693590d3678
rauzola <raul_sigoli@hotmail.com>
2021-05-23 23:17:14 -0300

    Sinalizando link ativo

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,38 @@
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+import { cloneElement, ReactElement } from 'react';
+
+interface ActiveLink extends LinkProps {
+  children: ReactElement
+  shouldMatchExactHref?: boolean
+}
+
+export function ActiveLink({ 
+  children, 
+  shouldMatchExactHref = false, 
+  ...rest 
+}: ActiveLink) {
+
+  const { asPath } = useRouter()
+
+  let isActive = false
+
+  if (shouldMatchExactHref && asPath === (rest.href || rest.as)) {
+    isActive = true
+  }
+
+  if (!shouldMatchExactHref && 
+    asPath.startsWith(String(rest.href) || String(rest.as))) {
+    isActive = true
+  }
+
+  return (
+    <Link {...rest}>
+      { 
+        cloneElement(children, {
+          color: isActive ? 'pink.400' : 'gray.50'
+        })
+      }
+    </Link>
+  )
+}
--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,20 +1,21 @@
-import { Link as ChakraLink, Text, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
-import { ElementType } from "react";
-import Link from 'next/link';
+import { ElementType } from 'react'
+import { Icon, Link, Text, LinkProps } from '@chakra-ui/react'
 
-interface NavLinkProps extends ChakraLinkProps {
-    icon: ElementType;
-    children: string;
-    href: string;
+import { ActiveLink } from '../ActiveLink'
+
+interface NavLinkProps extends LinkProps {
+  icon: ElementType
+  children: string
+  href: string
 }
 
-export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
-    return (
-        <Link href={href}>
-            <ChakraLink display="flex" align="center" {...rest} >
-                <Icon as={icon} fontSize="20" />
-                <Text ml="4" fontWeight="medium">{children}</Text>
-            </ChakraLink>
-        </Link>
-    );
+export function NavLink({icon, children, href, ...rest}: NavLinkProps) {
+  return (
+    <ActiveLink href={href} passHref>
+      <Link display='flex' align='center' {...rest}>
+        <Icon as={ icon } fontSize='20' />
+        <Text ml='4' fontWeight='medium'>{ children }</Text>
+      </Link>
+    </ActiveLink>
+  )
 }
