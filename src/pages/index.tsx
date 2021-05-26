1d6dd1e7593255a1413d0e9fc248a3f3caeb6f11
rauzola <raul_sigoli@hotmail.com>
2021-05-26 20:43:33 -0300

    Validação dos dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,28 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type SignInFormData = {
   email: string;
   password: string;
-};
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
 
-  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(values);
   }
 
   return (
@@ -34,41 +43,32 @@ export default function SignIn() {
         onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-
-          <Input
-            type="email"
-            name="email"
-            label="E-mail"
+          <Input 
+            name="email" 
+            type="email" 
+            label="E-mail" 
+            error={errors.email}
             {...register('email')}
           />
-
-          <Input
-            type="password"
-            name="password"
-            label="Senha"
+          <Input 
+            name="password" 
+            type="password" 
+            label="Senha" 
+            error={errors.password}
             {...register('password')}
           />
         </Stack>
 
-        <Button
-          type="submit"
-          mt="6"
-          colorScheme="pink"
-          isLoading={formState.isSubmitting}
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink" 
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
-
-
-
-
-
-
       </Flex>
-      {/* comenta essa linha depois */}
-      <Flex p="5">/dashboard</Flex>
     </Flex>
-
   )
 }
