e20f8b4233fa6729c06ce103237504d7a2fc0945
rauzola <raul_sigoli@hotmail.com>
2021-06-27 01:33:30 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-import {Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner} from '@chakra-ui/react'
-import {RiAddLine, RiPencilLine} from 'react-icons/ri';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { useQuery } from 'react-query';
 
-import {Header} from '../../components/Header/index';
+import { Header } from '../../components/Header/index';
 import { Pagination } from '../../components/Pagination';
 import React, { useEffect } from 'react';
 import { Sidebar } from '../../components/Sidebar';
@@ -13,8 +13,21 @@ export default function UserList() {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
+ 
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        });
 
-        return data;
+        return users;
     });
 
 
@@ -24,7 +37,7 @@ export default function UserList() {
         lg: true,
     })
 
-  
+
     return (
         <Box>
             <Header />
@@ -35,9 +48,9 @@ export default function UserList() {
                 <Box flex='1' borderRadius={8} bg='gray.800' p='8' >
                     <Flex mb='8' justify='space-between' align='center'>
                         <Heading size='lg' fontWeight='normal'>Usuários</Heading>
-                        
+
                         <Link href="/users/create">
-                            <Button 
+                            <Button
                                 as='a'
                                 size='sm'
                                 fontSize='sm'
@@ -50,7 +63,7 @@ export default function UserList() {
 
                     </Flex>
 
-                    { isLoading ? (
+                    {isLoading ? (
                         <Flex justify="center">
                             <Spinner />
                         </Flex>
@@ -60,47 +73,41 @@ export default function UserList() {
                         </Flex>
                     ) : (
                         <>
-                        <Table colorScheme='whiteAlpha'>
-                        <Thead>
-                            <Tr>
-                                <Th px={['4','4','6']} color='gray.300' width='8'>
-                                    <Checkbox colorScheme='pink'/>
-                                </Th>
-                                <Th>Usuário</Th>
-                                    { isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width='8'></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={['4','4', '6']}>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Raul Sigoli</Text>
-                                        <Text fontSize='sm' color='gray.300'>raul_sigoli@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                    { isWideVersion && <Td>27 de abril de 2021</Td>}
-                            </Tr>
-                            <Tr>
-                                <Td px={['4','4', '6']}>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Raul Sigoli</Text>
-                                        <Text fontSize='sm' color='gray.300'>raul_sigoli@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                    { isWideVersion && <Td>27 de abril de 2021</Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                            <Table colorScheme='whiteAlpha'>
+                                <Thead>
+                                    <Tr>
+                                        <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                                            <Checkbox colorScheme='pink' />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width='8'></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={['4', '4', '6']}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight='bold'>{user.name}</Text>
+                                                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                            </Tr>
+                                        )
+                                    })}
+
+                                </Tbody>
+                            </Table>
+                            <Pagination />
                         </>
-                    ) }
+                    )}
                 </Box>
             </Flex>
         </Box>
