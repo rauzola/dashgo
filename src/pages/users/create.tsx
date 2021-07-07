d8ed17a2f8dfe2d13070af547f3156212facef96
rauzola <raul_sigoli@hotmail.com>
2021-07-07 00:47:14 -0300

    Utilizando mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,107 +1,124 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, Stack } from '@chakra-ui/react'
-import Link from 'next/link'
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from 'react-hook-form'
-import * as yup from 'yup'
+import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMutation } from 'react-query'
 
-import { Input } from '../../components/Form/Input'
-import { Header } from '../../components/Header'
-import { Sidebar } from '../../components/Sidebar'
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
-  name: string
-  email: string
-  password: string
-  password_confirmation: string
-}
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
 
 const createUserFormSchema = yup.object().shape({
-  name: yup.string().required('Nome é obrigatório'),
-  email: yup.string().required('E-mail é obrigatório').email('E-mail inválido'),
-  password: yup.string().required('Senha é obrigatória').min(6, 'Mínimo 6 caracteres'),
-  password_confirmation: yup.string().oneOf(
-    [null, yup.ref('password')],
-    'As senhas precisam ser iguais'
-  )
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    null, yup.ref('password')
+  ], 'As senhas precisam ser iguais')
 })
 
 export default function CreateUser() {
-  const { register, handleSubmit, formState } = useForm<CreateUserFormData>({
+  const router = useRouter()
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
+
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
-  const { errors } = formState
 
-  console.log(errors)
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await createUser.mutateAsync(values);
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async values => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-
-    console.log(values)
+    router.push('/users')
   }
 
   return (
     <Box>
       <Header />
 
-      <Flex w='100%' my='6' mx='auto' maxWidth={1480} px='6'>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
         <Box
-          as='form'
-          flex='1'
+          as="form"
+          flex="1"
           borderRadius={8}
-          bg='gray.800'
-          p={['6', '8']}
+          bg="gray.800"
+          p={["6", "8"]}
           onSubmit={handleSubmit(handleCreateUser)}
         >
-          <Heading size='lg' fontWeight='normal'>Criar usuário</Heading>
+          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
-          <Divider my='6' borderColor='gray.700' />
+          <Divider my="6" borderColor="gray.700" />
 
-          <Stack spacing={['4', '6', '8']}>
-            <SimpleGrid minChildWidth='240px' spacing={['4', '6', '8']} w='100%'>
-              <Input 
-                name='name' 
-                label='Nome completo'
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="name"
+                label="Nome completo"
                 error={errors.name}
-                { ...register('name') }
+                {...register('name')}
               />
-              <Input 
-                type='email' 
-                name='email' 
-                label='E-mail' 
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
                 error={errors.email}
-                { ...register('email') }
+                {...register('email')}
               />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth='240px' spacing={['4', '6', '8']} w='100%'>
-              <Input 
-                type='password' 
-                name='password' 
-                label='Senha'
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
                 error={errors.password}
-                { ...register('password') }
+                {...register('password')}
               />
-              <Input 
-                type='password' 
-                name='password_confirmation' 
-                label='Confirmação da senha'
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
                 error={errors.password_confirmation}
-                { ...register('password_confirmation') }
+                {...register('password_confirmation')}
               />
             </SimpleGrid>
-          </Stack>
+          </VStack>
 
-          <Flex mt='8' justify='flex-end'>
-            <HStack spacing='4'>
-              <Link href='/users' passHref>
-                <Button as='a' colorScheme='whiteAlpha'>Cancelar</Button>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
               <Button
-                type='submit'
-                colorScheme='pink'
-                isLoading={ formState.isSubmitting }
+                type="submit"
+                colorScheme="pink"
+                isLoading={isSubmitting}
               >
                 Salvar
               </Button>
@@ -110,5 +127,5 @@ export default function CreateUser() {
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,66 +1,71 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
-import faker from 'faker';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs'
+import faker from 'faker'
 
 type User = {
-    name: string;
-    email: string;
-    created_at: string;
-}
+  name: string;
+  email: string;
+  created_at: string;
+};
 
 export function makeServer() {
-    const server = createServer({
+  const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
 
-        models: {
-            user: Model.extend<Partial<User>>({})
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`
         },
-
-        factories: {
-            user: Factory.extend({
-                name(i: number) {
-                    return `User ${i +1}`
-                },
-                email() {
-                    return faker.internet.email().toLocaleLowerCase();
-                },
-                createdAt() {
-                    return faker.date.recent(10);
-                },
-            })
+        email() {
+          return faker.internet.email().toLowerCase();
         },
-
-        seeds(server) {
-            server.createList('user', 200)
+        createdAt() {
+          return faker.date.recent(10);
         },
+      })
+    },
 
+    seeds(server) {
+      server.createList('user', 19)
+    },
 
-        routes() {
-            this.namespace = 'api';
-            this.timing = 750; //tempo de demora em mili segundos
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
 
-            this.get('/users', function (schema, request) {
-                const {page = 1, per_page = 10} = request.queryParams
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
 
-                const total = schema.all('user').length
+        const total = schema.all('user').length
 
-                const pageStart = (Number(page) - 1) * Number(per_page);
-                const pageEnd = pageStart + Number(per_page);
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
 
-                const users = this.serialize(schema.all('user')) 
-                    .users.slice(pageStart, pageEnd)
+        const users = this.serialize(schema.all('user'))
+          .users
+          .sort((a, b) => a.createdAt - b.createdAt)
+          .slice(pageStart, pageEnd)
 
-                return new Response(
-                    200,
-                    { 'x-total-count': String(total) },
-                    { users }
-                )
-            });
+        return new Response(
+          200,
+          { 'x-total-count': String(total) },
+          { users }
+        )
+      });
 
-            this.post('/users');
+      this.get('/users/:id');
+      this.post('/users');
 
-            this.namespace = '';
-            this.passthrough()
-        }
-    })
+      this.namespace = '';
+      this.passthrough()
+    }
+  })
 
-    return server;
+  return server;
 }
